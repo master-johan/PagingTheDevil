94e6f29b6ecef495ccdd6923f46b2e1df6b9aa3c
kallesongotto <47325654+kallesongotto@users.noreply.github.com>
2019-03-27 13:00:44 +0100

    Skapa rum

---
+++ Paging the devil/Paging the devil/GameObjects/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Paging_the_devil
+{
+    class Character : MovingObject
+    {
+        public Character(Texture2D tex, Vector2 pos, Rectangle rect) : base(tex, pos, rect)
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+
+}
---
+++ Paging the devil/Paging the devil/UtilityManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Paging_the_devil
+{
+    class UtilityManager
+    {
+        public static List<Texture2D> playerTextures = new List<Texture2D>();
+
+        public UtilityManager(ContentManager Content)
+        {
+            playerTextures.Add(Content.Load<Texture2D>(@"KnightSprite"));
+        }
+    }
+}
