da142732350148063061664f44f5989c68ac6f06
Johan <fritiofsson@gmail.com>
2019-03-29 16:02:04 +0100

    Fortsatt utveckling på slash abilityn

--- Paging_the_devil/Paging_the_devil/GameObject/Slash.cs
+++ Paging_the_devil/Paging_the_devil/GameObject/Slash.cs
@@ -10,17 +10,26 @@ namespace Paging_the_devil.GameObject
 {
     class Slash : Ability
     {
-        public Slash(Texture2D tex, Vector2 pos, Player player, Vector2 direction) 
+        Rectangle sourceRect;
+        float angle;
+        
+        public Slash(Texture2D tex, Vector2 pos, Player player, Vector2 direction, float angle) 
             : base (tex, pos, player, direction)
         {
-
+            this.angle = angle;
+            sourceRect = new Rectangle(0, 0, tex.Width, tex.Height);
         }
         public override void Update()
         {
-            if (player.slash)
-            {
+            angle += 0.3f;
+            //if (player.slash)
+            //{
 
-            }
+            //}
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tex, pos, sourceRect, Color.White, angle, new Vector2(0, tex.Height / 2), 1, SpriteEffects.None, 1);
         }
     }
 }
