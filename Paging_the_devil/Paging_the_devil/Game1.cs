ea18bd83a40112abf49a89ae9b253d988b304130
axlhammer <48758283+axlhammer@users.noreply.github.com>
2019-04-04 13:47:13 +0100

    merge

--- Paging_the_devil/Paging_the_devil/Game1.cs
+++ Paging_the_devil/Paging_the_devil/Game1.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace Paging_the_devil
 {
-
+    enum Room { One, Two, Three}
     public class Game1 : Game
     {
 
@@ -32,11 +32,119 @@ namespace Paging_the_devil
             TextureManager.LoadTextures(Content);
             gameManager = new GameManager(GraphicsDevice, graphics, this);
             
+
+            room = new Room(graphics);
+            
+
+
+
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
           
         }
         protected override void Update(GameTime gameTime)
         {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+            for (int i = 0; i < controllerArray.Length; i++)
+            {
+                if (connectedC[i].IsConnected && playerConnected[i] == false)
+                {
+                    playerArray[i] = new Player(TextureManager.playerTextureList[0], new Vector2(100 * i + 50, 100), new Rectangle(0, 0, 10, 10), i);
+
+
+                    playerConnected[i] = true;
+
+                }
+            }
+
+            for (int i = 0; i < nrOfPlayers; i++)
+            {
+                controllerArray[i].Update();
+                playerArray[i].Update();
+            }
+
+            for (int i = 0; i < nrOfPlayers; i++)
+            {
+                if (controllerArray[i].GetDirection() != Vector2.Zero)
+                {
+                    playerArray[i].LastInputDirection(controllerArray[i].GetDirection());
+                }
+                playerArray[i].InputDirection(controllerArray[i].GetDirection());
+                playerArray[i].InputPadState(controllerArray[i].GetPadState());
+            }
+
+
+            for (int i = 0; i < room.GetWallList().Count; i++)
+            {
+                for (int j = 0; j < nrOfPlayers; j++)
+                {
+                    if (playerArray[j].GetBotHitbox.Intersects(room.GetWallList()[i].GetRect))
+                    {
+
+                    }
+                    else if (playerArray[j].GetTopHitbox.Intersects(room.GetWallList()[i].GetRect))
+                    {
+
+                    }
+                    else if (playerArray[j].GetLeftHitbox.Intersects(room.GetWallList()[i].GetRect))
+                    {
+
+                    }
+                    else if (playerArray[j].GetRightHitbox.Intersects(room.GetWallList()[i].GetRect))
+                    {
+
+                    }
+                }
+            }
+
+            switch (currentRoom)
+            {
+                case Roomba.One:
+
+                    if (playerArray[0].GetRect.Intersects(portal.GetRect) && controllerArray[0].ButtonPressed(Buttons.Y))
+                    {
+
+                        //portal.GetSetPos = portalRoom2;
+                        currentRoom = Roomba.Two;
+                        SpawnEnemy();
+                    }
+
+                    break;
+                case Roomba.Two:
+                    if (playerArray[0].GetRect.Intersects(portal.GetRect) && controllerArray[0].ButtonPressed(Buttons.Y))
+                    {
+                        portal.GetSetPos = portalPos;
+                        currentRoom = Roomba.One;
+                    }
+                    else if (playerArray[0].GetRect.Intersects(portal2.GetRect) && controllerArray[0].ButtonPressed(Buttons.Y))
+                    {
+                        portal2.GetSetPos = portalRoom3;
+                        currentRoom = Roomba.Three;
+                        portal.GetSetPos = portalRoom4;
+                        SpawnEnemy();
+                    }
+                    break;
+                case Roomba.Three:
+                    if (playerArray[0].GetRect.Intersects(portal.GetRect) && controllerArray[0].ButtonPressed(Buttons.Y))
+                    {
+                        currentRoom = Roomba.Two;
+                        SpawnEnemy();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+                    foreach (var e in enemyList)
+                    {
+                        e.Update();
+
+                    }
+
+
             gameManager.Update(gameTime);
         }
         
